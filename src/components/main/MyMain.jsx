4f7538fcf81e4d4197f779388fa9d5240bdc9119
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-04-14 16:55:07 +0200

    updated

--- src/components/main/MyMain.jsx
+++ src/components/main/MyMain.jsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
+import MyLoader from "../MyLoader";
+import FolderIcon from "@mui/icons-material/Folder";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import ImageIcon from "@mui/icons-material/Image";
 
 function MyMain() {
-  const [allData, setAllData] = useState([]);
+  const [allFiles, setAllFiles] = useState([]);
+  const [directories, setDirectories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const fetchData = async () => {
     try {
-      let res = await fetch("");
+      let res = await fetch(
+        "https://fnp5vd20r2.execute-api.us-east-1.amazonaws.com/dev/directories"
+      );
+      if (res.ok) {
+        let data = await res.json();
+        setAllFiles(data.files);
+        setIsLoading(false);
+        setDirectories(data.directories);
+        console.log(data);
+      } else {
+        console.log("fetch data failed!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
-  useEffect(() => {}, []);
-  return <div>MyMain</div>;
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div>{isLoading ? <MyLoader /> : ""}</div>;
 }
 
 export default MyMain;
